b8df769ef498ab9ca307473d3dc56b2a95fbc473
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-11 00:30:52 +0100

    add safe area for mobile

--- app/(tabs)/items.tsx
+++ app/(tabs)/items.tsx
@@ -1,17 +1,20 @@
 import styled from "@emotion/native";
 import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ItemById } from "@/components/item/Item";
 import { ITEM_IDS } from "@/components/item/item.type";
 
 export default function ItemsScreen() {
   return (
-    <FlatList
-      data={ITEM_IDS}
-      renderItem={({ item }) => <ItemById id={item} />}
-      ItemSeparatorComponent={Separator}
-      keyExtractor={(item) => item}
-    />
+    <SafeAreaView>
+      <FlatList
+        data={ITEM_IDS}
+        renderItem={({ item }) => <ItemById id={item} />}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(item) => item}
+      />
+    </SafeAreaView>
   );
 }
 
