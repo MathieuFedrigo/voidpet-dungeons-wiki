c45142c80769390caf88f111bb3f582709820e1e
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-10 13:54:09 +0100

    use FlatList in item screen

--- app/(tabs)/items.tsx
+++ app/(tabs)/items.tsx
@@ -1,14 +1,18 @@
-import { ScrollView } from "react-native";
+import styled from "@emotion/native";
+import { FlatList } from "react-native";
 
 import { ItemById } from "@/components/item/Item";
 import { ITEM_IDS } from "@/components/item/item.type";
 
 export default function ItemsScreen() {
   return (
-    <ScrollView>
-      {ITEM_IDS.map((id) => (
-        <ItemById key={id} id={id} />
-      ))}
-    </ScrollView>
+    <FlatList
+      data={ITEM_IDS}
+      renderItem={({ item }) => <ItemById id={item} />}
+      ItemSeparatorComponent={Separator}
+      keyExtractor={(item) => item}
+    />
   );
 }
+
+const Separator = styled.View({ height: 16 });
