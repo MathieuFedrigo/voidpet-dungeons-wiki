7bef549367f944c165f310a2eec947e093b87e35
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-10 12:57:50 +0100

    render aii items

--- app/(tabs)/items.tsx
+++ app/(tabs)/items.tsx
@@ -1,11 +1,14 @@
 import { ScrollView } from "react-native";
 
 import { ItemById } from "@/components/item/Item";
+import { ITEM_IDS } from "@/components/item/item.type";
 
 export default function ItemsScreen() {
   return (
     <ScrollView>
-      <ItemById id="retroPinkFlowerCollar" />
+      {ITEM_IDS.map((id) => (
+        <ItemById key={id} id={id} />
+      ))}
     </ScrollView>
   );
 }
