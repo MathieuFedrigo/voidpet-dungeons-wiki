17b91bcc637153798eb1b2b5e86b339f103928bd
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-10 13:13:10 +0100

    add voidpetColors

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,5 +1,33 @@
+import { ItemRarity } from "@/components/item/item.type";
+
 const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
+
+const voidpetColors: Record<
+  ItemRarity,
+  { banner: string; itemBackground: string }
+> = {
+  common: {
+    banner: "#79CAB9",
+    itemBackground: "#B6E3D5",
+  },
+  rare: {
+    banner: "#85C5D7",
+    itemBackground: "#A8DCE1",
+  },
+  epic: {
+    banner: "#DC8BF3",
+    itemBackground: "#EFC8FB",
+  },
+  legendary: {
+    banner: "#DBA654",
+    itemBackground: "#F9DB6E",
+  },
+  uber: {
+    banner: "#5969E3",
+    itemBackground: "#444758",
+  },
+} as const;
 
 export const lightColors = {
   text: "#11181C",
@@ -8,6 +36,7 @@ export const lightColors = {
   icon: "#687076",
   tabIconDefault: "#687076",
   tabIconSelected: tintColorLight,
+  voidpet: voidpetColors,
 } as const;
 
 export const darkColors = {
@@ -17,4 +46,8 @@ export const darkColors = {
   icon: "#9BA1A6",
   tabIconDefault: "#9BA1A6",
   tabIconSelected: tintColorDark,
-} as const satisfies Record<keyof typeof lightColors, string>;
+  voidpet: voidpetColors,
+} as const satisfies Record<
+  keyof typeof lightColors,
+  string | typeof voidpetColors
+>;
