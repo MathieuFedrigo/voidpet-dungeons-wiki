3b27e2c63d2a3835b50c8a5e3835703b74691e9b
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-17 23:37:17 +0100

    add boss type and boss ids

---
+++ components/boss/boss.type.ts
@@ -0,0 +1,20 @@
+export type Boss = {
+  id: BossId;
+  name: string;
+};
+
+export const BOSS_IDS = [
+  "bogar",
+  "deathQuack",
+  "krakoth",
+  "shift",
+  "flint",
+  "vulko",
+  "voidweaver",
+  "harold",
+  "quasar",
+  "breakfastClub",
+  "galax",
+] as const;
+
+export type BossId = (typeof BOSS_IDS)[number];
