26da97ec9d250218ca8f744456a383cbcd7bc835
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-18 12:43:31 +0100

    add DROPS FROM section in SpecificItemScreen

--- app/(tabs)/item/[itemId].tsx
+++ app/(tabs)/item/[itemId].tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/native";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { getBoss, getBossIdsThatDropItem } from "@/components/boss/boss.config";
 import { ItemId } from "@/components/item/item.type";
 import { ItemById } from "@/components/item/ItemCard";
 
@@ -11,6 +12,12 @@ export default function SpecificItemScreen() {
   return (
     <Container>
       <ItemById id={itemId} disabled />
+      <DropsFromText>DROPS FROM:</DropsFromText>
+      <BossesText>
+        {getBossIdsThatDropItem(itemId)
+          .map((bossId) => getBoss(bossId).name)
+          .join(", ")}
+      </BossesText>
     </Container>
   );
 }
@@ -19,4 +26,18 @@ const Container = styled(SafeAreaView)(({ theme }) => ({
   flex: 1,
   padding: 12,
   backgroundColor: theme.colors.voidpet.basic.itemBackground,
+}));
+
+const DropsFromText = styled.Text(({ theme }) => ({
+  margin: 16,
+  fontSize: 16,
+  color: theme.colors.voidpet.basic.lightText,
+  alignSelf: "center",
+  fontWeight: "600",
+}));
+
+const BossesText = styled.Text(({ theme }) => ({
+  fontSize: 16,
+  color: theme.colors.voidpet.basic.lightText,
+  alignSelf: "center",
 }));
