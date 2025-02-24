3e75655d40b6c10d8080fd648e5d79381b7a0562
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-24 21:09:34 +0100

    memoize RiveView

--- components/ui/RiveView.tsx
+++ components/ui/RiveView.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
+import { memo } from "react";
 import Rive from "rive-react-native";
 
 import { RiveId } from "../boss/boss.type";
 
 type Props = { resourceName: RiveId; size: number };
-export const RiveView = ({ resourceName, size }: Props) => {
+export const RiveView = memo(({ resourceName, size }: Props) => {
   return (
     <Rive resourceName={resourceName} style={{ width: size, height: size }} />
   );
-};
+});
+RiveView.displayName = "RiveView";
--- components/ui/RiveView.web.tsx
+++ components/ui/RiveView.web.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 import Rive from "@rive-app/react-canvas";
+import { memo } from "react";
 
 import { getRiveResource } from "../boss/boss.rive";
 import { RiveId } from "../boss/boss.type";
 
 type Props = { resourceName: RiveId; size: number };
-export const RiveView = ({ resourceName, size }: Props) => (
+export const RiveView = memo(({ resourceName, size }: Props) => (
   <Rive
     src={getRiveResource(resourceName)}
     stateMachines="idle"
     style={{ width: size, height: size }}
   />
-);
+));
+RiveView.displayName = "RiveView";
