4bd2249311b20cffd07fae5eac95c0cad347ca6a
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-24 22:36:25 +0100

    add boss list in BossesScreen

--- app/(tabs)/boss/index.tsx
+++ app/(tabs)/boss/index.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable react-native/no-inline-styles */
 import styled from "@emotion/native";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { RiveView } from "@/components/ui/RiveView";
+import { BOSS_IDS } from "@/components/boss/boss.type";
+import { BossCardById } from "@/components/boss/BossCard";
 
 export default function BossesScreen() {
   return (
     <Container>
-      <RiveView resourceName="deathQuack" size={300} />
+      <FlatList
+        data={BOSS_IDS}
+        renderItem={({ item }) => <BossCardById id={item} />}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(item) => item}
+      />
     </Container>
   );
 }
+
+const Separator = styled.View({ height: 16 });
 
 const Container = styled(SafeAreaView)(({ theme }) => ({
   flex: 1,
