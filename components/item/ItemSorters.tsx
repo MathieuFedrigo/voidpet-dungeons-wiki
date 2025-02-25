08f3756bbab9dff58cc6e2f4230fa6616bd2afe9
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-25 19:46:13 +0100

    create ItemSorters

---
+++ components/item/ItemSorters.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/native";
+
+import { ItemStats } from "@/components/item/item.type";
+
+import { Collapsible } from "../ui/Collapsible";
+import { StatSelector } from "./ItemSelectors";
+
+interface ItemSortersProps {
+  selectedStat: keyof ItemStats;
+  toggleStat: (stat: keyof ItemStats) => void;
+}
+
+export const ItemSorters = ({ selectedStat, toggleStat }: ItemSortersProps) => {
+  return (
+    <Container>
+      <Collapsible title="Sorting">
+        <StatSelector
+          selectedStats={[selectedStat]}
+          toggleStat={toggleStat}
+          title="Sort by Stats"
+        />
+      </Collapsible>
+    </Container>
+  );
+};
+
+const Container = styled.View({
+  alignSelf: "center",
+  width: "100%",
+  maxWidth: 420,
+  marginBottom: 16,
+});
