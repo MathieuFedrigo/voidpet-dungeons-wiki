e73a6ffb52903ee40401c79732352bf62c07a76b
Mathieu Fedrigo <mathieuf@bam.tech>
2025-02-25 12:18:19 +0100

    update drops as item list in specific boss page

--- app/(tabs)/boss/[bossId].tsx
+++ app/(tabs)/boss/[bossId].tsx
@@ -1,27 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
 import styled from "@emotion/native";
 import { useLocalSearchParams } from "expo-router";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { getBoss } from "@/components/boss/boss.config";
 import { BossId } from "@/components/boss/boss.type";
 import { BossCardById } from "@/components/boss/BossCard";
-import { getItem } from "@/components/item/item.config";
+import { ItemById } from "@/components/item/ItemCard";
+import { Spacer } from "@/components/ui/Spacer";
 
 export default function SpecificBossScreen() {
   const { bossId } = useLocalSearchParams<{ bossId: BossId }>();
+  const drops = getBoss(bossId).drops;
   return (
     <Container>
       <BossCardById id={bossId} disabled />
       <DropsFromText>DROPS:</DropsFromText>
-      <BossesText>
-        {getBoss(bossId)
-          .drops.map((itemId) => getItem(itemId).name)
-          .join(", ")}
-      </BossesText>
+      <FlatList
+        data={drops}
+        renderItem={({ item }) => <ItemById id={item} />}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(item) => item}
+        ListFooterComponent={<Spacer vertical={64} />}
+      />
     </Container>
   );
 }
+
+const Separator = styled.View({ height: 16 });
 
 const Container = styled(SafeAreaView)(({ theme }) => ({
   flex: 1,
@@ -35,10 +42,4 @@ const DropsFromText = styled.Text(({ theme }) => ({
   color: theme.colors.voidpet.basic.lightText,
   alignSelf: "center",
   fontWeight: "600",
-}));
-
-const BossesText = styled.Text(({ theme }) => ({
-  fontSize: 16,
-  color: theme.colors.voidpet.basic.lightText,
-  alignSelf: "center",
 }));
