3ca1292bd3b45b8daa15e4f04b9dc0fa2346d8d1
Mathieu Fedrigo <mathieuf@bam.tech>
2025-03-04 13:09:08 +0100

    add vivid types

---
+++ components/vivid/vivid.type.ts
@@ -0,0 +1,28 @@
+export const VIVID_RARITIES = ["Rare", "Fabled", "Mythical", "Absurd"] as const;
+export type VividRarity = (typeof VIVID_RARITIES)[number];
+
+export const VIVID_TYPES = [
+  "Void",
+  "Wood",
+  "Fire",
+  "Water",
+  "Metal",
+  "Earth",
+] as const;
+export type VividType = (typeof VIVID_TYPES)[number];
+
+export type Vivid = {
+  name: VividName;
+  rarity: VividRarity;
+  colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+  };
+  description: string;
+  cost: number;
+  type: VividType;
+};
+
+export const VIVID_NAMES = ["Phantom"] as const;
+export type VividName = (typeof VIVID_NAMES)[number];
