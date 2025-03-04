29841591885c982d6e193b8e4f3649ad543f7e88
Mathieu Fedrigo <mathieuf@bam.tech>
2025-03-04 23:29:10 +0100

    add void vivid config

---
+++ components/vivid/vivid.config.ts
@@ -0,0 +1,199 @@
+import { Vivid, VividName } from "./vivid.type";
+
+export const VOID_VIVIDS_CONFIG: Record<VividName, Vivid> = {
+  Phantom: {
+    name: "Phantom",
+    rarity: "Rare",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 10,
+    description: "+5% speed",
+    type: "Void",
+  },
+  Ephemeral: {
+    name: "Ephemeral",
+    rarity: "Rare",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 15,
+    description: "+5% stamina",
+    type: "Void",
+  },
+  Celestial: {
+    name: "Celestial",
+    rarity: "Rare",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 20,
+    description: "+5% defense",
+    type: "Void",
+  },
+  Cataclysmic: {
+    name: "Cataclysmic",
+    rarity: "Rare",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 25,
+    description: "+5% attack",
+    type: "Void",
+  },
+
+  Jaded: {
+    name: "Jaded",
+    rarity: "Fabled",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 50,
+    description: "+5% healing effectiveness",
+    type: "Void",
+  },
+  Ethereal: {
+    name: "Ethereal",
+    rarity: "Fabled",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 75,
+    description: "+2% critical chance",
+    type: "Void",
+  },
+  Illusive: {
+    name: "Illusive",
+    rarity: "Fabled",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 100,
+    description: "+2.5% boss damage",
+    type: "Void",
+  },
+  Cryptic: {
+    name: "Cryptic",
+    rarity: "Fabled",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 125,
+    description: "+2.5% basic attack damage",
+    type: "Void",
+  },
+
+  Arcane: {
+    name: "Arcane",
+    rarity: "Mythical",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 270,
+    description: "+10% healing effectiveness",
+    type: "Void",
+  },
+  Resolute: {
+    name: "Resolute",
+    rarity: "Mythical",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 405,
+    description: "+5% boss damage",
+    type: "Void",
+  },
+  Fathomless: {
+    name: "Fathomless",
+    rarity: "Mythical",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 540,
+    description: "+10% critical damage",
+    type: "Void",
+  },
+  Elemental: {
+    name: "Elemental",
+    rarity: "Mythical",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 675,
+    description: "+2% armor penetration",
+    type: "Void",
+  },
+
+  Corrosive: {
+    name: "Corrosive",
+    rarity: "Absurd",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 1470,
+    description: "+20% first attack damage",
+    type: "Void",
+  },
+  Unidentified: {
+    name: "Unidentified",
+    rarity: "Absurd",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 2205,
+    description: "Reduce damage taken by 20% on the first hit",
+    type: "Void",
+  },
+  Intoxicating: {
+    name: "Intoxicating",
+    rarity: "Absurd",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 2940,
+    description: "Deal 3% of enemy's max health as bonus damage on each hit.",
+    type: "Void",
+  },
+  Tartarean: {
+    name: "Tartarean",
+    rarity: "Absurd",
+    colors: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+    cost: 3675,
+    description: "Gain 1% attack each turn",
+    type: "Void",
+  },
+};
