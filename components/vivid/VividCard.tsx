64f10f7fe35b9e0b889cd745f511f6d244140c6d
Mathieu Fedrigo <mathieuf@bam.tech>
2025-03-06 14:20:28 +0100

    improve VividCard

--- components/vivid/VividCard.tsx
+++ components/vivid/VividCard.tsx
@@ -1,3 +1,6 @@
+import styled from "@emotion/native";
+
+import { ItemRarity } from "../item/item.type";
 import { ThemedText } from "../ui/ThemedText";
 import { VOID_VIVIDS_CONFIG } from "./vivid.config";
 import { Vivid, VividName } from "./vivid.type";
@@ -12,16 +15,67 @@ const VividCard = ({
   vivid: Vivid;
 }) => {
   return (
-    <ThemedText>
-      {name}
-      {"\n"}
-      {description}
-      {"\n"}
-      Cost: {cost}
-      {"\n"}
-      Rarity: {rarity}
-      {"\n"}
-      Colors: {JSON.stringify(colors, null, 2)}
-    </ThemedText>
+    <>
+      <ThemedText>
+        {name}
+        {"\n"}
+        {description}
+        {"\n"}
+        Cost: {cost}
+        {"\n"}
+        Rarity: {rarity}
+        {"\n"}
+        Colors: {JSON.stringify(colors, null, 2)}
+      </ThemedText>
+      <Content>
+        <ItemInfo>
+          <ImageBackground rarity="common" />
+          <ItemFullDescription>
+            <ItemName>{name.toUpperCase()}</ItemName>
+            {description && <Description>{description}</Description>}
+          </ItemFullDescription>
+        </ItemInfo>
+      </Content>
+    </>
   );
 };
+
+const Content = styled.View(() => ({
+  backgroundColor: "#00000055",
+
+  minWidth: 220,
+  maxWidth: 320,
+  borderRadius: 16,
+  alignSelf: "center",
+  width: "100%",
+}));
+
+const ItemInfo = styled.View(() => ({
+  flexDirection: "row",
+}));
+
+const ItemFullDescription = styled.View(() => ({
+  padding: 16,
+  flex: 1,
+}));
+
+const ItemName = styled.Text(({ theme }) => ({
+  fontSize: 18,
+  fontWeight: "900",
+  color: theme.colors.voidpet.basic.dark,
+  marginBottom: 4,
+}));
+
+const Description = styled.Text(({ theme }) => ({
+  fontSize: 14,
+  color: theme.colors.voidpet.basic.dark,
+}));
+
+const ImageBackground = styled.View<{ rarity: ItemRarity }>(() => ({
+  width: 96,
+  height: 96,
+  backgroundColor: "#00000095",
+  borderTopLeftRadius: 16,
+  borderBottomLeftRadius: 16,
+  overflow: "hidden",
+}));
