022964a67357c9dc9849caee3410fc27bc15e0e9
Mathieu Fedrigo <mathieuf@bam.tech>
2025-03-07 17:18:13 +0100

    use flatlist for vivids

--- app/(tabs)/vivid/index.tsx
+++ app/(tabs)/vivid/index.tsx
@@ -1,19 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
 import styled from "@emotion/native";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { Spacer } from "@/components/ui/Spacer";
 import { VOID_VIVID_NAMES } from "@/components/vivid/vivid.config";
 import { VividCardById } from "@/components/vivid/VividCard";
 
 export default function VividsScreen() {
   return (
     <Container>
-      {VOID_VIVID_NAMES.map((name) => (
-        <VividCardById key={name} id={name} />
-      ))}
+      <FlatList
+        data={VOID_VIVID_NAMES}
+        renderItem={({ item }) => <VividCardById id={item} />}
+        keyExtractor={(item) => item}
+        ItemSeparatorComponent={Separator}
+        ListFooterComponent={<Spacer vertical={64} />}
+      />
     </Container>
   );
 }
+
+const Separator = styled.View({ height: 16 });
 
 const Container = styled(SafeAreaView)(({ theme }) => ({
   flex: 1,
