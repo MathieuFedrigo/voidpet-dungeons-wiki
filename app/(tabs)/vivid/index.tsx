9369ba68de1e2b482e9af11ed62548b8abc21766
Mathieu Fedrigo <mathieuf@bam.tech>
2025-03-07 17:48:49 +0100

    add columns for vivids

--- app/(tabs)/vivid/index.tsx
+++ app/(tabs)/vivid/index.tsx
@@ -6,8 +6,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Spacer } from "@/components/ui/Spacer";
 import { VOID_VIVID_NAMES } from "@/components/vivid/vivid.config";
 import { VividCardById } from "@/components/vivid/VividCard";
+import { useMaxNumColumns } from "@/hooks/useMaxNumColumns";
 
 export default function VividsScreen() {
+  const { numColumns, columnWrapperStyle } = useMaxNumColumns({
+    itemWidth: 320,
+    itemSpacing: 16,
+    maxColumns: 4,
+  });
+
   return (
     <Container>
       <FlatList
@@ -16,6 +23,9 @@ export default function VividsScreen() {
         keyExtractor={(item) => item}
         ItemSeparatorComponent={Separator}
         ListFooterComponent={<Spacer vertical={64} />}
+        numColumns={numColumns}
+        key={numColumns}
+        columnWrapperStyle={columnWrapperStyle}
       />
     </Container>
   );
